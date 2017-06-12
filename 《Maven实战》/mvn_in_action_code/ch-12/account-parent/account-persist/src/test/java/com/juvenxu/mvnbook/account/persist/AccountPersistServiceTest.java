db5bdbf4ae88fe7cb71c17b706974f65e664605f
Nakajima Shigeharu <shigeharu_1978@yahoo.co.jp>
2017-06-13 05:37:37 +0900

    account-persist test source modify

--- 《Maven实战》/mvn_in_action_code/ch-12/account-parent/account-persist/src/test/java/com/juvenxu/mvnbook/account/persist/AccountPersistServiceTest.java
+++ 《Maven实战》/mvn_in_action_code/ch-12/account-parent/account-persist/src/test/java/com/juvenxu/mvnbook/account/persist/AccountPersistServiceTest.java
@@ -9,94 +9,82 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AccountPersistServiceTest
-{
+public class AccountPersistServiceTest {
 	private AccountPersistService service;
 
 	@Before
-	public void prepare()
-		throws Exception
-	{
-		File persistDataFile = new File ( "target/test-classes/persist-data.xml" );
+	public void prepare() throws Exception {
+		File persistDataFile = new File("target/test-classes/persist-data.xml","");
 
-		if ( persistDataFile.exists() )
-		{
+		if (persistDataFile.exists()) {
 			persistDataFile.delete();
 		}
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext( "account-persist.xml" );
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("account-persist.xml");
 
-		service = (AccountPersistService) ctx.getBean( "accountPersistService" );
+		service = (AccountPersistService) ctx.getBean("accountPersistService");
 
-    	Account account = new Account();
-    	account.setId("juven");
-    	account.setName("Juven Xu");
-    	account.setEmail("juven@changeme.com");
-    	account.setPassword("this_should_be_encrypted");
-    	account.setActivated(true);
+		Account account = new Account();
+		account.setId("juven");
+		account.setName("Juven Xu");
+		account.setEmail("juven@changeme.com");
+		account.setPassword("this_should_be_encrypted");
+		account.setActivated(true);
 
-    	service.createAccount(account);
+		service.createAccount(account);
 	}
 
-    @Test
-    public void testReadAccount()
-        throws Exception
-    {
-        Account account = service.readAccount( "juven" );
+	@Test
+	public void testReadAccount() throws Exception {
+		Account account = service.readAccount("juven");
 
-        assertNotNull( account );
-        assertEquals( "juven", account.getId() );
-        assertEquals( "Juven Xu", account.getName() );
-        assertEquals( "juven@changeme.com", account.getEmail() );
-        assertEquals( "this_should_be_encrypted", account.getPassword() );
-        assertTrue( account.isActivated() );
-    }
+		assertNotNull(account);
+		assertEquals("juven", account.getId());
+		assertEquals("Juven Xu", account.getName());
+		assertEquals("juven@changeme.com", account.getEmail());
+		assertEquals("this_should_be_encrypted", account.getPassword());
+		assertTrue(account.isActivated());
+	}
 
-//    @Test
-//    public void testDeleteAccount()
-//        throws Exception
-//    {
-//        assertNotNull( service.readAccount( "juven" ) );
-//        service.deleteAccount( "juven" );
-//        assertNull( service.readAccount( "juven" ) );
-//    }
-//
-//    @Test
-//    public void testCreateAccount()
-//    	throws Exception
-//    {
-//    	assertNull( service.readAccount( "mike" ) );
-//
-//    	Account account = new Account();
-//    	account.setId("mike");
-//    	account.setName("Mike");
-//    	account.setEmail("mike@changeme.com");
-//    	account.setPassword("this_should_be_encrypted");
-//    	account.setActivated(true);
-//
-//    	service.createAccount(account);
-//
-//    	assertNotNull( service.readAccount( "mike" ));
-//    }
-//
-//    @Test
-//    public void testUpdateAccount()
-//    	throws Exception
-//    {
-//    	Account account = service.readAccount( "juven" );
-//
-//    	account.setName("Juven Xu 1");
-//    	account.setEmail("juven1@changeme.com");
-//    	account.setPassword("this_still_should_be_encrypted");
-//    	account.setActivated(false);
-//
-//    	service.updateAccount( account );
-//
-//    	account = service.readAccount( "juven" );
-//
-//        assertEquals( "Juven Xu 1", account.getName() );
-//        assertEquals( "juven1@changeme.com", account.getEmail() );
-//        assertEquals( "this_still_should_be_encrypted", account.getPassword() );
-//        assertFalse( account.isActivated() );
-//    }
+	@Test
+	public void testDeleteAccount() throws Exception {
+		assertNotNull(service.readAccount("juven"));
+		service.deleteAccount("juven");
+		assertNull(service.readAccount("juven"));
+	}
+
+	@Test
+	public void testCreateAccount() throws Exception {
+		assertNull(service.readAccount("mike"));
+
+		Account account = new Account();
+		account.setId("mike");
+		account.setName("Mike");
+		account.setEmail("mike@changeme.com");
+		account.setPassword("this_should_be_encrypted");
+		account.setActivated(true);
+
+		service.createAccount(account);
+
+		assertNotNull(service.readAccount("mike"));
+	}
+
+	@Test
+	public void testUpdateAccount() throws Exception {
+		Account account = service.readAccount("juven");
+
+		account.setName("Juven Xu 1");
+		account.setEmail("juven1@changeme.com");
+		account.setPassword("this_still_should_be_encrypted");
+		account.setActivated(false);
+
+		service.updateAccount(account);
+
+		account = service.readAccount("juven");
+
+		assertEquals("Juven Xu 1", account.getName());
+		assertEquals("juven1@changeme.com", account.getEmail());
+		assertEquals("this_still_should_be_encrypted", account.getPassword());
+		assertFalse(account.isActivated());
+	}
 }
