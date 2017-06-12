0f15225079f83ee0590c64d13d430f32290c54f4
Nakajima Shigeharu <shigeharu_1978@yahoo.co.jp>
2017-06-13 05:11:53 +0900

    テストできるように修正
    Signed-off-by: Nakajima Shigeharu <shigeharu_1978@yahoo.co.jp>

--- 《Maven实战》/mvn_in_action_code/ch-12/account-parent/account-captcha/src/test/java/com/juvenxu/mvnbook/account/captcha/AccountCaptchaServiceTest.java
+++ 《Maven实战》/mvn_in_action_code/ch-12/account-parent/account-captcha/src/test/java/com/juvenxu/mvnbook/account/captcha/AccountCaptchaServiceTest.java
@@ -3,6 +3,7 @@ package com.juvenxu.mvnbook.account.captcha;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -13,73 +14,69 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AccountCaptchaServiceTest
-{
-    private AccountCaptchaService service;
+public class AccountCaptchaServiceTest {
+	private AccountCaptchaService service;
 
-    @Before
-    public void prepare()
-        throws Exception
-    {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext( "account-captcha.xml" );
-        service = (AccountCaptchaService) ctx.getBean( "accountCaptchaService" );
-    }
+	@Before
+	public void prepare() throws Exception {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("account-captcha.xml");
+		service = (AccountCaptchaService) ctx.getBean("accountCaptchaService");
+	}
 
-    @Test
-    public void testGenerateCaptcha()
-        throws Exception
-    {
-        String captchaKey = service.generateCaptchaKey();
-        assertNotNull( captchaKey );
+	@Test
+	public void testGenerateCaptcha() throws Exception {
+		String captchaKey = service.generateCaptchaKey();
+		assertNotNull(captchaKey);
 
-        byte[] captchaImage = service.generateCaptchaImage( captchaKey );
-        assertTrue( captchaImage.length > 0 );
+		byte[] captchaImage = service.generateCaptchaImage(captchaKey);
+		assertTrue(captchaImage.length > 0);
 
-        File image = new File( "target/" + captchaKey + ".jpg" );
-        OutputStream output = null;
-        try
-        {
-            output = new FileOutputStream( image );
-            output.write( captchaImage );
-        }
-        finally
-        {
-            if ( output != null )
-            {
-                output.close();
-            }
-        }
-        assertTrue( image.exists() && image.length() > 0 );
-    }
+		createImage(captchaKey, captchaImage);
 
-    @Test
-    public void testValidateCaptchaCorrect()
-        throws Exception
-    {
-        List<String> preDefinedTexts = new ArrayList<String>();
-        preDefinedTexts.add( "12345" );
-        preDefinedTexts.add( "abcde" );
-        service.setPreDefinedTexts( preDefinedTexts );
+	}
 
-        String captchaKey = service.generateCaptchaKey();
-        service.generateCaptchaImage( captchaKey );
-        assertTrue( service.validateCaptcha( captchaKey, "12345" ) );
+	@Test
+	public void testValidateCaptchaCorrect() throws Exception {
+		List<String> preDefinedTexts = new ArrayList<String>();
+		preDefinedTexts.add("12345");
+		preDefinedTexts.add("abcde");
+		service.setPreDefinedTexts(preDefinedTexts);
 
-        captchaKey = service.generateCaptchaKey();
-        service.generateCaptchaImage( captchaKey );
-        assertTrue( service.validateCaptcha( captchaKey, "abcde" ) );
-    }
+		String captchaKey = service.generateCaptchaKey();
+		byte[] captchaImage = service.generateCaptchaImage(captchaKey);
+		assertTrue(service.validateCaptcha(captchaKey, "12345"));
 
-    @Test
-    public void testValidateCaptchaIncorrect()
-        throws Exception
-    {
-        List<String> preDefinedTexts = new ArrayList<String>();
-        preDefinedTexts.add( "12345" );
-        service.setPreDefinedTexts( preDefinedTexts );
+		createImage(captchaKey, captchaImage);
 
-        String captchaKey = service.generateCaptchaKey();
-        service.generateCaptchaImage( captchaKey );
-        assertFalse( service.validateCaptcha( captchaKey, "67890" ) );
-    }
+		captchaKey = service.generateCaptchaKey();
+		captchaImage = service.generateCaptchaImage(captchaKey);
+		assertTrue(service.validateCaptcha(captchaKey, "abcde"));
+
+		createImage(captchaKey, captchaImage);
+	}
+
+	@Test
+	public void testValidateCaptchaIncorrect() throws Exception {
+		List<String> preDefinedTexts = new ArrayList<String>();
+		preDefinedTexts.add("12345");
+		service.setPreDefinedTexts(preDefinedTexts);
+
+		String captchaKey = service.generateCaptchaKey();
+		service.generateCaptchaImage(captchaKey);
+		assertFalse(service.validateCaptcha(captchaKey, "67890"));
+	}
+
+	private void createImage(String captchaKey, byte[] captchaImage) throws Exception {
+		File image = new File("target/" + captchaKey + ".jpg");
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream(image);
+			output.write(captchaImage);
+		} finally {
+			if (output != null) {
+				output.close();
+			}
+		}
+		assertTrue(image.exists() && image.length() > 0);
+	}
 }
